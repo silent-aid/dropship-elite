6a2037db0aecfefe7919d9028df1cbb1bfe90677
Yahya <moderngreengaming@gmail.com>
2025-04-24 20:21:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import Contact from './pages/Contact';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"; // Import Navbar
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Contact from "./pages/Contact";
 
-function App() {
+const App = () => {
   return (
-    <Router>
+    <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </div>
   );
-}
+};
 
 export default App;
