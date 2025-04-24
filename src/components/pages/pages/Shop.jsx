5288f142425482883340480fefc815709e153f63
Yahya <moderngreengaming@gmail.com>
2025-04-24 20:05:52 +0530

    Create Shop.jsx

---
+++ src/components/pages/pages/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Shop = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Shop</h1>
+      <p>Explore trending designs for your store.</p>
+    </div>
+  );
+};
+
+export default Shop;
