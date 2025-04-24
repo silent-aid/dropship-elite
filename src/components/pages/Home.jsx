a132057e544f2d8cc120adc065c5627090555216
Yahya <moderngreengaming@gmail.com>
2025-04-24 20:05:37 +0530

    Create Home.jsx

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Welcome to Wear Your Voice</h1>
+      <p>Your dropshipping hub for powerful designs!</p>
+    </div>
+  );
+};
+
+export default Home;
